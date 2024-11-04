da2c304247208dfcaa795888ee71bbf3e5b11590
raretypeoffox <133606685+raretypeoffox@users.noreply.github.com>
2024-11-04 11:12:34 -0800

    Code review notes (Main and Init

--- Scripts/Core/Modules/AdventureMode/Init.lua
+++ Scripts/Core/Modules/AdventureMode/Init.lua
@@ -1,3 +1,7 @@
+-- CODEREVIEW: This is good. I'd recommend using this script to setup all your default variables
+-- However, make sure to put it at the very top of your package so it's the first script to load.
+-- That ensures that the other scripts will all be able to find AshraelPackage
+
 -- Persistent adventure mode state table
 AshraelPackage = AshraelPackage or {}
 AshraelPackage.AdventureMode = AshraelPackage.AdventureMode or {}
